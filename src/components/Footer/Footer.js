057e8b64d9e44033175e59c6a4665b9328566c07
Andy <andrew.marshall1@outlook.com>
2019-03-05 21:41:27 -0700

    inital

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">Andy Marshall</div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import spiderman from './images/spiderman.png';
+import venom from './images/venom.png';
+import doc_ock from './images/doc_ock.png';
+import green_goblin from './images/green_goblin.png';
+import miles from './images/miles.png';
+import spider_ham from './images/spider_ham.png';
+import sandman from './images/sandman.png';
+import rhino from './images/rhino.png';
+import noir from './images/noir.png';
+import gwen from './images/gwen.png';
+import kingpin from './images/kingpin.png';
+import spiderman2 from './images/spiderman2.png';
+
+const images = [
+  spiderman,
+  venom,
+  doc_ock,
+  green_goblin,
+  miles,
+  spider_ham,
+  sandman,
+  rhino,
+  noir,
+  gwen,
+  kingpin,
+  spiderman2
+];
+
+export default images;
