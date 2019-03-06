c80577def82eff9ba92947b0d6ff2ab575bc0fa4
Andy <andrew.marshall1@outlook.com>
2019-03-05 22:02:43 -0700

    Updates

---
+++ precache-manifest.0aba92a812d478978a195100c69f26d1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d12c43cf392fa138aaf810e26cea059a",
+    "url": "/react-clicky-game/static/media/spider_ham.d12c43cf.png"
+  },
+  {
+    "revision": "4a3c71642640192885bf",
+    "url": "/react-clicky-game/static/css/main.381893d3.chunk.css"
+  },
+  {
+    "revision": "07c95f7184c1a76ef7a1",
+    "url": "/react-clicky-game/static/js/runtime~main.07c95f71.js"
+  },
+  {
+    "revision": "26b6f5e32e72b688440b",
+    "url": "/react-clicky-game/static/js/2.26b6f5e3.chunk.js"
+  },
+  {
+    "revision": "52fc5ec5a071f5dce60592b158def664",
+    "url": "/react-clicky-game/static/media/spiderman.52fc5ec5.png"
+  },
+  {
+    "revision": "0eb868cdaf548284cfb90ef36616b8e0",
+    "url": "/react-clicky-game/static/media/venom.0eb868cd.png"
+  },
+  {
+    "revision": "9dda4761a483ecffc43fd5f5a83fd14d",
+    "url": "/react-clicky-game/static/media/doc_ock.9dda4761.png"
+  },
+  {
+    "revision": "40ab3542f30e3e78a9aeb9861e08f737",
+    "url": "/react-clicky-game/static/media/green_goblin.40ab3542.png"
+  },
+  {
+    "revision": "5cf30092cf977b51f12316fb8e19479e",
+    "url": "/react-clicky-game/static/media/miles.5cf30092.png"
+  },
+  {
+    "revision": "4a3c71642640192885bf",
+    "url": "/react-clicky-game/static/js/main.4a3c7164.chunk.js"
+  },
+  {
+    "revision": "17bee2059cf561c91d7117b65ce50be2",
+    "url": "/react-clicky-game/static/media/sandman.17bee205.png"
+  },
+  {
+    "revision": "fcd52468caa869eab82d06db20dff056",
+    "url": "/react-clicky-game/static/media/rhino.fcd52468.png"
+  },
+  {
+    "revision": "07bce146bff9425f0eb3ca62d8370dc7",
+    "url": "/react-clicky-game/static/media/noir.07bce146.png"
+  },
+  {
+    "revision": "75c17b218f921ca570c2281311311107",
+    "url": "/react-clicky-game/static/media/gwen.75c17b21.png"
+  },
+  {
+    "revision": "2672ba1aa3d3ddbd992d2334e0ff8d6c",
+    "url": "/react-clicky-game/static/media/kingpin.2672ba1a.png"
+  },
+  {
+    "revision": "ec6e51ef11ee2fb3b2a365c8551c5d79",
+    "url": "/react-clicky-game/static/media/spiderman2.ec6e51ef.png"
+  },
+  {
+    "revision": "3164b2c52de593ed6aeefbcb7d88f086",
+    "url": "/react-clicky-game/static/media/bg.3164b2c5.png"
+  },
+  {
+    "revision": "17d28a9b8be14cdd798be0e9a282361e",
+    "url": "/react-clicky-game/index.html"
+  }
+];
